4d006b0588b0b4d413af34ae8d2a6cdb2b2a2244
Siddesh <siddeshsankhya@gmail.com>
2023-02-18 00:20:10 +0530

    contracts

--- src/App.js
+++ src/App.js
@@ -6,9 +6,37 @@ import Invest from "./components/Invest";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import AssetPage from "./pages/AssetPage";
+import { Chain } from "wagmi";
+
+export const canto = {
+  id: 740,
+  name: "Canto Testnet",
+  network: "Canto Testnet`",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Canto",
+    symbol: "CANTO",
+  },
+  rpcUrls: {
+    public: { http: ["https://eth.plexnode.wtf"] },
+    default: { http: ["https://eth.plexnode.wtf"] },
+  },
+  blockExplorers: {
+    etherscan: {
+      name: "Canto",
+      url: "https://testnet-explorer.canto.neobase.one/",
+    },
+    default: {
+      name: "Canto",
+      url: "https://testnet-explorer.canto.neobase.one/",
+    },
+  },
+};
+const chains = [canto];
 const client = createClient(
   getDefaultClient({
     appName: "Canto Index",
+    chains,
   })
 );
 
